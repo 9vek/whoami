f42763c27c0d7c83d0e6c7dca9393cf38973a673
Kevyn <39913345+KevynTang@users.noreply.github.com>
2022-11-20 18:29:02 +0800

    front-end page styling

--- src/whoami_frontend/src/App.jsx
+++ src/whoami_frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react"
-import { render } from "react-dom"
+import { createRoot } from "react-dom/client"
 import "./index.css"
 
-const Container = () => {
+class Container extends React.Component {
 
-  return (
-    <div className="container mx-auto min-h-screen max-w-2xl m-1 p-1 bg-stone-100">
-      Hello React! 
-    </div>
-  )
+
+
+  render() {
+    return (
+      <div className="container mx-auto max-w-2xl m-4 p-1 bg-purple-50 shadow-lg rounded-lg grid grid-cols-1 place-items-center">
+        <div>
+          <div className="text-stone-900 text-4xl font-bold text-center m-16">WHO AM I?</div>
+          <div className="cursor-pointer text-white text-2xl font-bold px-4 py-2 m-4 w-fit mx-auto bg-purple-600 hover:bg-purple-500 shadow-md rounded-md">Login</div>
+        </div>
+      </div>
+    )
+  }
 
 }
 
 
-render(<Container />, document.getElementById("app"))
+const root = createRoot(document.getElementById("app"))
+root.render(<Container />)
