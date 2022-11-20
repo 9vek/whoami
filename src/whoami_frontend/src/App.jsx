77c900a12d68757c7a950daadb233bc120121780
Kevyn <39913345+KevynTang@users.noreply.github.com>
2022-11-20 18:12:38 +0800

    Tailwind installation

--- src/whoami_frontend/src/App.jsx
+++ src/whoami_frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { render } from "react-dom"
+import "../assets/index.css"
 
 const Container = () => {
+
   return (
-    <div>
+    <div className="container mx-auto min-h-screen max-w-2xl m-1 p-1 bg-stone-100">
       Hello React! 
     </div>
   )
+
 }
+
 
 render(<Container />, document.getElementById("app"))
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/whoami_frontend/**/*.{html,js,jsx}'],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
